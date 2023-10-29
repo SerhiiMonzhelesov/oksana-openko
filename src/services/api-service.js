b717f98d07996b60c1f829e48931cc8f0e12c3b5
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-10-29 11:11:24 +0200

    fixed

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://oksana-openko.onreder.com';
-// 'https://localhost:3000';
+// 'http://localhost:3000';
 
 export async function addContact(formData) {
   const options = {
@@ -9,13 +9,68 @@ export async function addContact(formData) {
     headers: {
       'Content-Type': 'application/json',
     },
+
     data: formData,
   };
 
-  const data = await axios
-    .post(`${BASE_URL}/api/application`, formData, options)
-    .then(response => response);
-  // .catch(error => console.log('error: ', error));
+  const { data } = await axios.post(
+    `${BASE_URL}/api/application`,
+    formData,
+    options
+  );
 
   return data;
 }
+
+// export async function addUser(user) {
+//   const options = {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     data: user,
+//   };
+
+//   const { data } = await axios.post(
+//     'https://goit-node-js-backend.onrender.com/api/users/register',
+//     user,
+//     options
+//   );
+
+//   console.log('data: ', data);
+// }
+
+// const testURL = 'https://oksana-openko.onreder.com/api/application';
+
+// function addApply(formData) {
+//   const myInit = {
+//     method: 'POST',
+// mode: 'cors',
+// credentials: 'include',
+// headers: {
+// 'Access-Control-Allow-Origin': '*',
+// 'Content-Type': 'application/json',
+// },
+//   body: JSON.stringify(formData),
+// };
+
+//   const myRequest = new Request(testURL, myInit);
+//   fetch(myRequest)
+//     .then(function (response) {
+//       return response;
+//     })
+//     .then(function (response) {
+//       console.log(response);
+//     })
+//     .catch(function (e) {
+//       console.log(e);
+//     });
+// }
+
+// addApply({
+//   name: 'Serhii',
+//   phone: '+380668572712',
+//   service: 'Психотерапія',
+//   format: 'Онлайн',
+//   question: 'new test postman to web',
+// });
